28aa386e654212cdb06e823fc416195090976745
Jahan Mohammad <jzmohama@cougarnet.uh.edu>
2023-02-23 23:28:31 -0600

    1

--- src/lib/components/login.tsx
+++ src/lib/components/login.tsx
@@ -51,7 +51,8 @@ export const Login = () => {
                 name="password"
                 type="text"
                 placeholder="Password"/>
-            <button type="submit">Submit</button>
+            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
+
             </form>
         </div>
     )
